Fixed missing conj in Fredholm1 Adjoint with on-the-fly computation was bugged for complex convolution kernel as it was missing conj. We also added a test to ensure this error would be catched.

pylops/signalprocessing/Fredholm1.py
@@ -115,5 +115,5 @@ class Fredholm1(LinearOperator):
                     y[isl] = np.dot(self.GT[isl], x[isl])
             else:
                 for isl in range(self.nsl):
-                    y[isl] = np.dot(self.G[isl].T, x[isl])
+                    y[isl] = np.dot(self.G[isl].conj().T, x[isl])
         return y.ravel()