14efa791872c653610a43225f580fdaa27ccb6f8
mrava87 <matteoravasi@gmail.com>
2020-06-06 16:50:12 +0200

    Added warning to Sliding2D and Sliding3D

--- pylops/signalprocessing/Sliding3D.py
+++ pylops/signalprocessing/Sliding3D.py
@@ -33,6 +33,12 @@ def Sliding3D(Op, dims, dimsd, nwin, nover, nop,
        choice ``dims`` and use the number of windows printed on screen to
        define such input parameter.
 
+    .. warning:: Depending on the choice of `nwin` and `nover` as well as the
+       size of the data, sliding windows may not cover the entire first and/or
+       second dimensions. The start and end indeces of each window can be
+       displayed using ``design=True`` while defining the best sliding window
+       approach.
+
     Parameters
     ----------
     Op : :obj:`pylops.LinearOperator`
