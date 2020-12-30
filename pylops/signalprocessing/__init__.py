576cf93dc16d88fe7e9b4e7fae2a8f1cefe1aa8f
mrava87 <matteoravasi@gmail.com>
2020-12-30 17:47:53 +0100

    Added Sliding1D and Patch2D to init

--- pylops/signalprocessing/__init__.py
+++ pylops/signalprocessing/__init__.py
@@ -8,8 +8,10 @@ from .Interp import Interp
 from .Bilinear import Bilinear
 from .Radon2D import Radon2D
 from .Radon3D import Radon3D
+from .Sliding1D import Sliding1D
 from .Sliding2D import Sliding2D
 from .Sliding3D import Sliding3D
+from .Patch2D import Patch2D
 from .Fredholm1 import Fredholm1
 from .DWT import DWT
 from .DWT2D import DWT2D
