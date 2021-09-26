a64c07e9359165cb2aaf3a9807a3a669cc15b878
mrava87 <matteoravasi@gmail.com>
2021-09-26 21:02:19 +0300

    Bump smoothing tests to float64

--- pytests/test_smoothing.py
+++ pytests/test_smoothing.py
@@ -14,13 +14,15 @@ par4 = {'nz': 11, 'ny': 51, 'nx': 31, 'dir':1} # odd, second direction
 par5 = {'nz': 10, 'ny': 30, 'nx': 20, 'dir':2} # even, third direction
 par6 = {'nz': 11, 'ny': 51, 'nx': 31, 'dir':2} # odd, third direction
 
+np.random.seed(0)
+
 
 @pytest.mark.parametrize("par", [(par1), (par2), (par3), (par4)])
 def test_Smoothing1D(par):
     """Dot-test and inversion for smoothing
     """
     # 1d kernel on 1d signal
-    D1op = Smoothing1D(nsmooth=5, dims=par['nx'], dtype='float32')
+    D1op = Smoothing1D(nsmooth=5, dims=par['nx'], dtype='float64')
     assert dottest(D1op, par['nx'], par['nx'])
 
     x = np.random.normal(0, 1, par['nx'])
@@ -30,7 +32,7 @@ def test_Smoothing1D(par):
 
     # 1d kernel on 2d signal
     D1op = Smoothing1D(nsmooth=5, dims=(par['ny'], par['nx']),
-                       dir=par['dir'], dtype='float32')
+                       dir=par['dir'], dtype='float64')
     assert dottest(D1op, par['ny']*par['nx'], par['ny']*par['nx'])
 
     x = np.random.normal(0, 1, (par['ny'], par['nx'])).flatten()
@@ -40,7 +42,7 @@ def test_Smoothing1D(par):
 
     # 1d kernel on 3d signal
     D1op = Smoothing1D(nsmooth=5, dims=(par['nz'], par['ny'], par['nx']),
-                       dir=par['dir'], dtype='float32')
+                       dir=par['dir'], dtype='float64')
     assert dottest(D1op, par['nz'] * par['ny'] * par['nx'],
                    par['nz'] * par['ny'] * par['nx'], tol=1e-3)
 
@@ -58,7 +60,7 @@ def test_Smoothing2D(par):
     # 2d kernel on 2d signal
     if par['dir'] < 2:
         D2op = Smoothing2D(nsmooth=(5, 5), dims=(par['ny'], par['nx']),
-                           dtype='float32')
+                           dtype='float64')
         assert dottest(D2op, par['ny']*par['nx'], par['ny']*par['nx'], tol=1e-3)
 
         # forward
@@ -78,7 +80,7 @@ def test_Smoothing2D(par):
 
     # 2d kernel on 3d signal
     D2op = Smoothing2D(nsmooth=(5, 5), dims=(par['nz'], par['ny'], par['nx']),
-                       nodir=par['dir'], dtype='float32')
+                       nodir=par['dir'], dtype='float64')
     assert dottest(D2op, par['nz'] * par['ny'] * par['nx'],
                    par['nz'] * par['ny'] * par['nx'])
 
