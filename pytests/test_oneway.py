e1e695309bac518af11212303848ee30367ab698
mrava87 <matteoravasi@gmail.com>
2022-05-02 16:47:11 +0300

    fix: Deghosing tutorial broken after v1.17.0 Fixes a bug introduced in Deghosting method when updating the FFT class. A small change is also introduced in the unittest to make it more robust (it would have now failed with the introduced bug). See https://github.com/PyLops/pylops/issues/377 for details.

--- pytests/test_oneway.py
+++ pytests/test_oneway.py
@@ -101,11 +101,9 @@ def test_Deghosting_2dsignal(par, create_data2D):
         vel_sep,
         zrec,
         win=np.ones_like(p2d),
-        npad=0,
-        ntaper=0,
+        npad=3,
+        ntaper=5,
         dtype=par["dtype"],
         **dict(damp=1e-10, iter_lim=60)
     )
-
-    print(np.linalg.norm(p2d_minus_inv - p2d_minus) / np.linalg.norm(p2d_minus))
     assert np.linalg.norm(p2d_minus_inv - p2d_minus) / np.linalg.norm(p2d_minus) < 3e-1
