e0471ac5baeff63e529dbf7f9260dbf5c7d7bbdc
aniket singh rawat <aniket.rawat@bytelearn.ai>
2023-02-18 11:45:04 +0530

    fixed dct tests

--- pytests/test_dct.py
+++ pytests/test_dct.py
@@ -13,21 +13,21 @@ par3 = {"ny": 21, "nx": 21, "imag": 0, "dtype": "float64"}
 def test_DCT1D(par):
     """Dot test for Discrete Cosine Transform Operator 1D"""
 
-    t = np.arange(par["ny"])
+    t = np.arange(par["ny"]) + 1
 
     for type in [1, 2, 3, 4]:
         Dct = DCT(dims=(par["ny"],), type=type, dtype=par["dtype"])
         assert dottest(Dct, par["ny"], par["ny"], rtol=1e-6, complexflag=0, verb=True)
 
         y = Dct.H * (Dct * t)
-        np.allclose(t, y)
+        np.testing.assert_allclose(t, y)
 
 
 @pytest.mark.parametrize("par", [(par1), (par2), (par3)])
 def test_DCT2D(par):
     """Dot test for Discrete Cosine Transform Operator 2D"""
 
-    t = np.outer(np.arange(par["ny"]), np.arange(par["nx"]))
+    t = np.outer(np.arange(par["ny"]) + 1, np.arange(par["nx"]) + 1)
 
     for type in [1, 2, 3, 4]:
         for axes in [0, 1]:
@@ -42,7 +42,7 @@ def test_DCT2D(par):
             )
 
             y = Dct.H * (Dct * t)
-            np.allclose(t, y)
+            np.testing.assert_allclose(t, y)
 
 
 @pytest.mark.parametrize("par", [(par1), (par2), (par3)])
@@ -64,16 +64,16 @@ def test_DCT3D(par):
             )
 
             y = Dct.H * (Dct * t)
-            np.allclose(t, y)
+            np.testing.assert_allclose(t, y)
 
 
 @pytest.mark.parametrize("par", [(par1), (par3)])
 def test_DCT_workers(par):
     """Dot test for Discrete Cosine Transform Operator with workers"""
-    t = np.arange(par["ny"])
+    t = np.arange(par["ny"]) + 1
 
     Dct = DCT(dims=(par["ny"],), type=1, dtype=par["dtype"], workers=2)
     assert dottest(Dct, par["ny"], par["ny"], rtol=1e-6, complexflag=0, verb=True)
 
     y = Dct.H * (Dct * t)
-    np.allclose(t, y)
+    np.testing.assert_allclose(t, y)
