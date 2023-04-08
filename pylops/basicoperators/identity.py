6180b5afdcefb4ca34cb1a8c0036b0f2265fea56
mrava87 <matteoravasi@gmail.com>
2023-04-08 22:32:39 +0300

    feature: added ndarray capabilities to Identity

--- pylops/basicoperators/identity.py
+++ pylops/basicoperators/identity.py
@@ -1,13 +1,14 @@
 __all__ = ["Identity"]
 
 
-from typing import Optional
+from typing import Optional, Union
 
 import numpy as np
 
 from pylops import LinearOperator
 from pylops.utils.backend import get_array_module
-from pylops.utils.typing import DTypeLike, NDArray
+from pylops.utils.decorators import reshaped
+from pylops.utils.typing import DTypeLike, InputDimsLike, NDArray
 
 
 class Identity(LinearOperator):
@@ -19,16 +20,29 @@ class Identity(LinearOperator):
     removes last :math:`N - M` elements from data in adjoint and pads with
     :math:`0` in forward.
 
+    Note that the identity operator can handle both 1d and nd arrays; in the
+    case of nd arrays, all elements of N must be larger or equal than those of M
+    (or all elements of M must be larger or equal than those of N).
+
     Parameters
     ----------
-    N : :obj:`int`
+    N : :obj:`int` or :obj:`tuple`
         Number of samples in data (and model, if ``M`` is not provided).
-    M : :obj:`int`, optional
-        Number of samples in model.
+        If a tuple is provided, this is interpreted as the data (and model)
+        are nd-arrays.
+    M : :obj:`int` or :obj:`tuple`, optional
+        Number of samples in model. If a tuple is provided, this is interpreted
+        as the model is an nd-array. Note that when `M` is a tuple, `N` must be
+        also a tuple with the same number of elements.
     inplace : :obj:`bool`, optional
         Work inplace (``True``) or make a new copy (``False``). By default,
         data is a reference to the model (in forward) and model is a reference
         to the data (in adjoint).
+    forceflat : :obj:`bool`, optional
+         .. versionadded:: 2.2.0
+
+         Force an array to be flattened after matvec and rmatvec. Note that this is only
+         required when `N` and `M` are tuples (input and output arrays are nd-arrays.
     dtype : :obj:`str`, optional
         Type of elements in input array.
     name : :obj:`str`, optional
@@ -43,6 +57,15 @@ class Identity(LinearOperator):
     explicit : :obj:`bool`
         Operator contains a matrix that can be solved explicitly (``True``) or
         not (``False``)
+
+    Raises
+    ------
+    ValueError
+        - If ``M`` is a tuple with different number of elements of ``N``
+        - If ``N`` ``M`` are non-identical tuples and some values are largers
+          in ``N`` and some in ``M``
+    NotImplemented
+        If ``N`` or ``M`` have type different from int or tuple/list
 
     Notes
     -----
@@ -87,38 +110,90 @@ class Identity(LinearOperator):
 
     def __init__(
         self,
-        N: int,
-        M: Optional[int] = None,
+        N: Union[int, InputDimsLike],
+        M: Optional[Union[int, InputDimsLike]] = None,
         inplace: bool = True,
+        forceflat: bool = None,
         dtype: DTypeLike = "float64",
         name: str = "I",
     ) -> None:
         M = N if M is None else M
-        super().__init__(dtype=np.dtype(dtype), shape=(N, M), name=name)
+        if isinstance(N, int) and isinstance(M, int):
+            # N and M are scalars (1d-arrays)
+            super().__init__(
+                dtype=np.dtype(dtype),
+                dims=(M,),
+                dimsd=(N,),
+                forceflat=forceflat,
+                name=name,
+            )
+            # identify behaviour for matvec/rmatvec: 'same' for N=M,
+            # 'data' for N>M, and 'model' for M>N
+            if N == M:
+                self.mode = "same"
+            elif N < M:
+                self.mode = "model"
+                self.sliceN = slice(0, N)
+                self.sliceM = slice(0, M)
+            else:
+                self.mode = "data"
+                self.sliceN = slice(0, N)
+                self.sliceM = slice(0, M)
+        elif isinstance(N, (tuple, list)) and isinstance(M, (tuple, list)):
+            # N and M are tuples (nd-arrays)
+            # First check that all elements in N and M are the same or that
+            # all elements of either N or M are bigger than the other one and
+            # raise error is not the case
+            if np.array_equal(N, M):
+                self.mode = "same"
+            elif np.array_equal(M, np.maximum(N, M)):
+                self.mode = "model"
+                self.sliceN = tuple([slice(0, n) for n in N])
+                self.sliceM = tuple([slice(0, m) for m in M])
+            elif np.array_equal(N, np.maximum(N, M)):
+                self.mode = "data"
+                self.sliceN = tuple([slice(0, n) for n in N])
+                self.sliceM = tuple([slice(0, m) for m in M])
+            else:
+                raise ValueError(
+                    "N and M are not identical, "
+                    "and some values are larger in N and some in M"
+                )
+            super().__init__(
+                dtype=np.dtype(dtype), dims=M, dimsd=N, forceflat=forceflat, name=name
+            )
+        else:
+            raise NotImplemented(
+                f"N and M must have same type and equal to "
+                f"int, tuple, or list, instead their types"
+                f" are type(N)={type(N)} and type(M)={type(M)}"
+            )
         self.inplace = inplace
 
+    @reshaped
     def _matvec(self, x: NDArray) -> NDArray:
         ncp = get_array_module(x)
         if not self.inplace:
             x = x.copy()
-        if self.shape[0] == self.shape[1]:
+        if self.mode == "same":
             y = x
-        elif self.shape[0] < self.shape[1]:
-            y = x[: self.shape[0]]
+        elif self.mode == "model":
+            y = x[self.sliceN]
         else:
-            y = ncp.zeros(self.shape[0], dtype=self.dtype)
-            y[: self.shape[1]] = x
+            y = ncp.zeros(self.dimsd, dtype=self.dtype)
+            y[self.sliceM] = x
         return y
 
+    @reshaped
     def _rmatvec(self, x: NDArray) -> NDArray:
         ncp = get_array_module(x)
         if not self.inplace:
             x = x.copy()
-        if self.shape[0] == self.shape[1]:
+        if self.mode == "same":
             y = x
-        elif self.shape[0] < self.shape[1]:
-            y = ncp.zeros(self.shape[1], dtype=self.dtype)
-            y[: self.shape[0]] = x
+        elif self.mode == "model":
+            y = ncp.zeros(self.dims, dtype=self.dtype)
+            y[self.sliceN] = x
         else:
-            y = x[: self.shape[1]]
+            y = x[self.sliceM]
         return y
