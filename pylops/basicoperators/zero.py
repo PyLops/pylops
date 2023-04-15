200973f9cc5dcc8b329033a4b57694c6647a5e97
mrava87 <matteoravasi@gmail.com>
2023-04-15 23:36:40 +0300

    feature: allow NDarray for Zero

--- pylops/basicoperators/zero.py
+++ pylops/basicoperators/zero.py
@@ -1,12 +1,12 @@
 __all__ = ["Zero"]
 
-from typing import Optional
+from typing import Optional, Union
 
 import numpy as np
 
 from pylops import LinearOperator
 from pylops.utils.backend import get_array_module
-from pylops.utils.typing import DTypeLike, NDArray
+from pylops.utils.typing import DTypeLike, InputDimsLike, NDArray
 
 
 class Zero(LinearOperator):
@@ -17,10 +17,14 @@ class Zero(LinearOperator):
 
     Parameters
     ----------
-    N : :obj:`int`
-       Number of samples in data (and model in M is not provided).
-    M : :obj:`int`, optional
-       Number of samples in model.
+    N : :obj:`int` or :obj:`tuple`
+        Number of samples in data (and model, if ``M`` is not provided).
+        If a tuple is provided, this is interpreted as the data (and model)
+        are nd-arrays.
+    M : :obj:`int` or :obj:`tuple`, optional
+        Number of samples in model. If a tuple is provided, this is interpreted
+        as the model is an nd-array. Note that when `M` is a tuple, `N` must be
+        also a tuple with the same number of elements.
     dtype : :obj:`str`, optional
        Type of elements in input array.
     name : :obj:`str`, optional
@@ -55,13 +59,33 @@ class Zero(LinearOperator):
 
     def __init__(
         self,
-        N: int,
-        M: Optional[int] = None,
+        N: Union[int, InputDimsLike],
+        M: Optional[Union[int, InputDimsLike]] = None,
+        forceflat: bool = None,
         dtype: DTypeLike = "float64",
         name: str = "Z",
     ) -> None:
         M = N if M is None else M
-        super().__init__(dtype=np.dtype(dtype), shape=(N, M), name=name)
+        if isinstance(N, int) and isinstance(M, int):
+            # N and M are scalars (1d-arrays)
+            super().__init__(
+                dtype=np.dtype(dtype),
+                dims=(M,),
+                dimsd=(N,),
+                forceflat=forceflat,
+                name=name,
+            )
+        elif isinstance(N, (tuple, list)) and isinstance(M, (tuple, list)):
+            # N and M are tuples (nd-arrays)
+            super().__init__(
+                dtype=np.dtype(dtype), dims=M, dimsd=N, forceflat=forceflat, name=name
+            )
+        else:
+            raise NotImplementedError(
+                f"N and M must have same type and equal to "
+                f"int, tuple, or list, instead their types"
+                f" are type(N)={type(N)} and type(M)={type(M)}"
+            )
 
     def _matvec(self, x: NDArray) -> NDArray:
         ncp = get_array_module(x)
