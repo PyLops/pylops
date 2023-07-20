65dcd9c656a97963f9e174890d8ca929883ea5c7
mrava87 <matteoravasi@gmail.com>
2023-07-20 09:13:17 +0300

    minor: fixed flake8 issues

--- pylops/signalprocessing/_nonstatconvolve3d_cuda.py
+++ pylops/signalprocessing/_nonstatconvolve3d_cuda.py
@@ -4,7 +4,9 @@ from numba import cuda
 
 
 @cuda.jit(max_registers=40)
-def _matvec_rmatvec(x, y, hs, hshape, xdims, ohx, ohy, ohz, dhx, dhy, dhz, nhx, nhy, nhz, rmatvec):
+def _matvec_rmatvec(
+    x, y, hs, hshape, xdims, ohx, ohy, ohz, dhx, dhy, dhz, nhx, nhy, nhz, rmatvec
+):
     """Cuda kernels for NonStationaryConvolve3D operator
 
     Cuda implementation of matvec and rmatvec for NonStationaryConvolve3D operator. See
@@ -14,16 +16,16 @@ def _matvec_rmatvec(x, y, hs, hshape, xdims, ohx, ohy, ohz, dhx, dhy, dhz, nhx, 
     ix, iy, iz = cuda.grid(3)
 
     if ix < xdims[0] and iy < xdims[1] and iz < xdims[2]:
-        
+
         ## find closest filters and interpolate h
-        ihx_l = int(floor((ix - ohx) / dhx)) #id number of left for hs_arr
-        ihy_b = int(floor((iy - ohy) / dhy)) #id number of back for hs_arr  
-        ihz_t = int(floor((iz - ohz) / dhz)) #id number of top  for hs_arr
-        
-        dhx_r = (ix - ohx) / dhx - ihx_l #weight for right psfs, left 1-dhx_r
-        dhy_f = (iy - ohy) / dhy - ihy_b #weight for front psfs, back 1-dhy_f
-        dhz_d = (iz - ohz) / dhz - ihz_t #weight for down psfs,  top 1-dhz_d
-        
+        ihx_l = int(floor((ix - ohx) / dhx))  # id number of left for hs_arr
+        ihy_b = int(floor((iy - ohy) / dhy))  # id number of back for hs_arr
+        ihz_t = int(floor((iz - ohz) / dhz))  # id number of top  for hs_arr
+
+        dhx_r = (ix - ohx) / dhx - ihx_l  # weight for right psfs, left 1-dhx_r
+        dhy_f = (iy - ohy) / dhy - ihy_b  # weight for front psfs, back 1-dhy_f
+        dhz_d = (iz - ohz) / dhz - ihz_t  # weight for down psfs,  top 1-dhz_d
+
         if ihx_l < 0:
             ihx_l = ihx_r = 0
             dhx_l = dhx_r = 0.5
@@ -33,7 +35,7 @@ def _matvec_rmatvec(x, y, hs, hshape, xdims, ohx, ohy, ohz, dhx, dhy, dhz, nhx, 
         else:
             ihx_r = ihx_l + 1
             dhx_l = 1.0 - dhx_r
-            
+
         if ihy_b < 0:
             ihy_b = ihy_f = 0
             dhy_b = dhy_f = 0.5
@@ -58,13 +60,13 @@ def _matvec_rmatvec(x, y, hs, hshape, xdims, ohx, ohy, ohz, dhx, dhy, dhz, nhx, 
         h_lbd = hs[ihx_l, ihy_b, ihz_d]
         h_lft = hs[ihx_l, ihy_f, ihz_t]
         h_lfd = hs[ihx_l, ihy_f, ihz_d]
-        
+
         h_rbt = hs[ihx_r, ihy_b, ihz_t]
         h_rbd = hs[ihx_r, ihy_b, ihz_d]
         h_rft = hs[ihx_r, ihy_f, ihz_t]
         h_rfd = hs[ihx_r, ihy_f, ihz_d]
 
-       ## find extremes of model where to apply h (in case h is going out of model)
+        ## find extremes of model where to apply h (in case h is going out of model)
         xextremes = (
             max(0, ix - hshape[0] // 2),
             min(ix + hshape[0] // 2 + 1, xdims[0]),
@@ -77,8 +79,7 @@ def _matvec_rmatvec(x, y, hs, hshape, xdims, ohx, ohy, ohz, dhx, dhy, dhz, nhx, 
             max(0, iz - hshape[2] // 2),
             min(iz + hshape[2] // 2 + 1, xdims[2]),
         )
-        
-        
+
         ## find extremes of h (in case h is going out of model)
         hxextremes = (
             max(0, -ix + hshape[0] // 2),
@@ -92,7 +93,7 @@ def _matvec_rmatvec(x, y, hs, hshape, xdims, ohx, ohy, ohz, dhx, dhy, dhz, nhx, 
             max(0, -iz + hshape[2] // 2),
             min(hshape[2], hshape[2] // 2 + (xdims[2] - iz)),
         )
-        
+
         # place filter in output
         for ixx, hxx in zip(
             range(xextremes[0], xextremes[1]), range(hxextremes[0], hxextremes[1])
@@ -105,7 +106,7 @@ def _matvec_rmatvec(x, y, hs, hshape, xdims, ohx, ohy, ohz, dhx, dhy, dhz, nhx, 
                     range(hzextremes[0], hzextremes[1]),
                 ):
                     h = (
-                          dhx_l * dhy_b * dhz_t * h_lbt[hxx, hyy, hzz]
+                        dhx_l * dhy_b * dhz_t * h_lbt[hxx, hyy, hzz]
                         + dhx_l * dhy_b * dhz_d * h_lbd[hxx, hyy, hzz]
                         + dhx_l * dhy_f * dhz_t * h_lft[hxx, hyy, hzz]
                         + dhx_l * dhy_f * dhz_d * h_lfd[hxx, hyy, hzz]
@@ -114,7 +115,7 @@ def _matvec_rmatvec(x, y, hs, hshape, xdims, ohx, ohy, ohz, dhx, dhy, dhz, nhx, 
                         + dhx_r * dhy_f * dhz_t * h_rft[hxx, hyy, hzz]
                         + dhx_r * dhy_f * dhz_d * h_rfd[hxx, hyy, hzz]
                     )
-                    
+
                     if rmatvec:
                         y[ix, iy, iz] += h * x[ixx, iyy, izz]
                     else:
@@ -136,9 +137,9 @@ def _matvec_rmatvec_call(
     nhx,
     nhy,
     nhz,
-    rmatvec= False,
-    num_blocks=(1,32,16),
-    num_threads_per_blocks=(24,24,24),
+    rmatvec=False,
+    num_blocks=(1, 32, 16),
+    num_threads_per_blocks=(24, 24, 24),
 ):
     """Caller for NonStationaryConvolve3D operator
 
--- pylops/signalprocessing/nonstatconvolve3d.py
+++ pylops/signalprocessing/nonstatconvolve3d.py
@@ -41,7 +41,8 @@ class NonStationaryConvolve3D(LinearOperator):
         Number of samples for each dimension (which we refer to as :math`n_x \times n_y \times n_z`).
     hs : :obj:`numpy.ndarray`
         Bank of 3d compact filters of size
-        :math:`n_{\text{filts},x} \times n_{\text{filts},y} \times n_{\text{filts},z} \times n_{h,x} \times n_{h,y} \times n_{h,z}`.
+        :math:`n_{\text{filts},x} \times n_{\text{filts},y} \times
+        n_{\text{filts},z} \times n_{h,x} \times n_{h,y} \times n_{h,z}`.
         Filters must have odd number of samples and are assumed to be
         centered in the middle of the filter support.
     ihx : :obj:`tuple`
@@ -83,7 +84,8 @@ class NonStationaryConvolve3D(LinearOperator):
     -----
     See :class:`pylops.signalprocessing.NonStationaryConvolve2D`.
 
-    """    
+    """
+
     def __init__(
         self,
         dims: Union[int, InputDimsLike],
@@ -100,34 +102,51 @@ class NonStationaryConvolve3D(LinearOperator):
             raise NotImplementedError("engine must be numpy or numba or cuda")
         if hs.shape[3] % 2 == 0 or hs.shape[4] % 2 == 0 or hs.shape[5] % 2 == 0:
             raise ValueError("filters hs must have odd length")
-        if len(np.unique(np.diff(ihx))) > 1 or len(np.unique(np.diff(ihy))) > 1 or len(np.unique(np.diff(ihz))) > 1:
+        if (
+            len(np.unique(np.diff(ihx))) > 1
+            or len(np.unique(np.diff(ihy))) > 1
+            or len(np.unique(np.diff(ihz))) > 1
+        ):
             raise ValueError(
                 "the indices of filters 'ih' are must be regularly sampled"
             )
-        if min(ihx) < 0 or min(ihy) < 0 or min(ihz) < 0 or max(ihx) >= dims[0] or max(ihy) >= dims[1] or max(ihz) >= dims[2]:
+        if (
+            min(ihx) < 0
+            or min(ihy) < 0
+            or min(ihz) < 0
+            or max(ihx) >= dims[0]
+            or max(ihy) >= dims[1]
+            or max(ihz) >= dims[2]
+        ):
             raise ValueError(
                 "the indices of filters 'ih' must be larger than 0 and smaller than `dims`"
             )
         self.hs = hs
         self.hshape = hs.shape[3:]
-        self.ohx, self.dhx, self.nhx = ihx[0], ihx[1] - ihx[0], len(ihx) 
-        self.ohy, self.dhy, self.nhy = ihy[0], ihy[1] - ihy[0], len(ihy) 
-        self.ohz, self.dhz, self.nhz = ihz[0], ihz[1] - ihz[0], len(ihz) 
+        self.ohx, self.dhx, self.nhx = ihx[0], ihx[1] - ihx[0], len(ihx)
+        self.ohy, self.dhy, self.nhy = ihy[0], ihy[1] - ihy[0], len(ihy)
+        self.ohz, self.dhz, self.nhz = ihz[0], ihz[1] - ihz[0], len(ihz)
         self.ehx, self.ehx, self.ehz = ihx[-1], ihy[-1], ihz[-1]
         self.dims = _value_or_sized_to_tuple(dims)
         self.engine = engine
         super().__init__(dtype=np.dtype(dtype), dims=dims, dimsd=dims, name=name)
-        
+
         # create additional input parameters for engine=cuda
         self.kwargs_cuda = {}
         if engine == "cuda":
             self.kwargs_cuda["num_threads_per_blocks"] = num_threads_per_blocks
-            num_blocks_x = (self.dims[0] + num_threads_per_blocks[0] - 1) // num_threads_per_blocks[0]
-            num_blocks_y = (self.dims[1] + num_threads_per_blocks[1] - 1) // num_threads_per_blocks[1]
-            num_blocks_z = (self.dims[2] + num_threads_per_blocks[2] - 1) // num_threads_per_blocks[2]
+            num_blocks_x = (
+                self.dims[0] + num_threads_per_blocks[0] - 1
+            ) // num_threads_per_blocks[0]
+            num_blocks_y = (
+                self.dims[1] + num_threads_per_blocks[1] - 1
+            ) // num_threads_per_blocks[1]
+            num_blocks_z = (
+                self.dims[2] + num_threads_per_blocks[2] - 1
+            ) // num_threads_per_blocks[2]
             self.kwargs_cuda["num_blocks"] = (num_blocks_x, num_blocks_y, num_blocks_z)
         self._register_multiplications(engine)
-        
+
     def _register_multiplications(self, engine: str) -> None:
         if engine == "numba":
             numba_opts = dict(nopython=True, fastmath=True, nogil=True, parallel=True)
@@ -136,7 +155,7 @@ class NonStationaryConvolve3D(LinearOperator):
             self._mvrmv = _matvec_rmatvec_cuda_call
         else:
             self._mvrmv = self._matvec_rmatvec
-    
+
     @staticmethod
     def _matvec_rmatvec(
         x: NDArray,
@@ -159,14 +178,26 @@ class NonStationaryConvolve3D(LinearOperator):
             for iy in range(xdims[1]):
                 for iz in range(xdims[2]):
                     # find closest filters and interpolate h
-                    ihx_l = int(np.floor((ix - ohx) / dhx)) # id number of left for hs_arr
-                    ihy_b = int(np.floor((iy - ohy) / dhy)) # id number of back for hs_arr
-                    ihz_t = int(np.floor((iz - ohz) / dhz)) # id number of top  for hs_arr
-                    
-                    dhx_r = (ix - ohx) / dhx - ihx_l # weight for right psfs, left 1-ihz_t
-                    dhy_f = (iy - ohy) / dhy - ihy_b # weight for front psfs, left 1-ihz_t
-                    dhz_d = (iz - ohz) / dhz - ihz_t # weight for down psfs,  top 1-dhz_d
-                    
+                    ihx_l = int(
+                        np.floor((ix - ohx) / dhx)
+                    )  # id number of left for hs_arr
+                    ihy_b = int(
+                        np.floor((iy - ohy) / dhy)
+                    )  # id number of back for hs_arr
+                    ihz_t = int(
+                        np.floor((iz - ohz) / dhz)
+                    )  # id number of top  for hs_arr
+
+                    dhx_r = (
+                        ix - ohx
+                    ) / dhx - ihx_l  # weight for right psfs, left 1-ihz_t
+                    dhy_f = (
+                        iy - ohy
+                    ) / dhy - ihy_b  # weight for front psfs, left 1-ihz_t
+                    dhz_d = (
+                        iz - ohz
+                    ) / dhz - ihz_t  # weight for down psfs,  top 1-dhz_d
+
                     if ihx_l < 0:
                         ihx_l = ihx_r = 0
                         dhx_l = dhx_r = 0.5
@@ -176,7 +207,7 @@ class NonStationaryConvolve3D(LinearOperator):
                     else:
                         ihx_r = ihx_l + 1
                         dhx_l = 1.0 - dhx_r
-                        
+
                     if ihy_b < 0:
                         ihy_b = ihy_f = 0
                         dhy_b = dhy_f = 0.5
@@ -186,7 +217,7 @@ class NonStationaryConvolve3D(LinearOperator):
                     else:
                         ihy_f = ihy_b + 1
                         dhy_b = 1.0 - dhy_f
-    
+
                     if ihz_t < 0:
                         ihz_t = ihz_d = 0
                         dhz_t = dhz_d = 0.5
@@ -196,19 +227,19 @@ class NonStationaryConvolve3D(LinearOperator):
                     else:
                         ihz_d = ihz_t + 1
                         dhz_t = 1.0 - dhz_d
-    
+
                     h_lbt = hs[ihx_l, ihy_b, ihz_t]
                     h_lbd = hs[ihx_l, ihy_b, ihz_d]
                     h_lft = hs[ihx_l, ihy_f, ihz_t]
                     h_lfd = hs[ihx_l, ihy_f, ihz_d]
-                    
+
                     h_rbt = hs[ihx_r, ihy_b, ihz_t]
                     h_rbd = hs[ihx_r, ihy_b, ihz_d]
                     h_rft = hs[ihx_r, ihy_f, ihz_t]
                     h_rfd = hs[ihx_r, ihy_f, ihz_d]
-    
+
                     h = (
-                          dhx_l * dhy_b * dhz_t * h_lbt
+                        dhx_l * dhy_b * dhz_t * h_lbt
                         + dhx_l * dhy_b * dhz_d * h_lbd
                         + dhx_l * dhy_f * dhz_t * h_lft
                         + dhx_l * dhy_f * dhz_d * h_lfd
@@ -217,7 +248,7 @@ class NonStationaryConvolve3D(LinearOperator):
                         + dhx_r * dhy_f * dhz_t * h_rft
                         + dhx_r * dhy_f * dhz_d * h_rfd
                     )
-    
+
                     # find extremes of model where to apply h (in case h is going out of model)
                     xextremes = (
                         max(0, ix - hshape[0] // 2),
@@ -245,19 +276,35 @@ class NonStationaryConvolve3D(LinearOperator):
                         max(0, -iz + hshape[2] // 2),
                         min(hshape[2], hshape[2] // 2 + (xdims[2] - iz)),
                     )
-                    
+
                     if not rmatvec:
-                        y[ xextremes[0] : xextremes[1], yextremes[0] : yextremes[1], zextremes[0] : zextremes[1] ] += (
+                        y[
+                            xextremes[0] : xextremes[1],
+                            yextremes[0] : yextremes[1],
+                            zextremes[0] : zextremes[1],
+                        ] += (
                             x[ix, iy, iz]
-                            * h[ hxextremes[0] : hxextremes[1], hyextremes[0] : hyextremes[1], hzextremes[0] : hzextremes[1] ]
+                            * h[
+                                hxextremes[0] : hxextremes[1],
+                                hyextremes[0] : hyextremes[1],
+                                hzextremes[0] : hzextremes[1],
+                            ]
                         )
                     else:
                         y[ix, iy, iz] = np.sum(
-                            h[ hxextremes[0] : hxextremes[1], hyextremes[0] : hyextremes[1], hzextremes[0] : hzextremes[1] ]
-                            * x[ xextremes[0] : xextremes[1], yextremes[0] : yextremes[1], zextremes[0] : zextremes[1] ]
+                            h[
+                                hxextremes[0] : hxextremes[1],
+                                hyextremes[0] : hyextremes[1],
+                                hzextremes[0] : hzextremes[1],
+                            ]
+                            * x[
+                                xextremes[0] : xextremes[1],
+                                yextremes[0] : yextremes[1],
+                                zextremes[0] : zextremes[1],
+                            ]
                         )
-        return y 
-    
+        return y
+
     @reshaped
     def _matvec(self, x: NDArray) -> NDArray:
         ncp = get_array_module(x)
