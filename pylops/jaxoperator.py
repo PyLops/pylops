0ef312f495de8061b81ba50eb8d8d31c0c7ebaaf
mrava87 <matteoravasi@gmail.com>
2024-06-21 21:22:24 +0300

    doc: added docstring and types to jaxoperator

--- pylops/jaxoperator.py
+++ pylops/jaxoperator.py
@@ -2,18 +2,46 @@ __all__ = [
     "JaxOperator",
 ]
 
-
-import jax
+from typing import Any, NewType
 
 from pylops import LinearOperator
+from pylops.utils import deps
+
+if deps.jax_enabled:
+    import jax
+    import jaxlib
+
+    jaxarray_type = jaxlib.xla_extension.ArrayImpl
+else:
+    jaxarray_type = Any
+
+JaxType = NewType("JaxType", jaxarray_type)
 
 
 class JaxOperator(LinearOperator):
-    def __init__(self, Op):
+    def __init__(self, Op: LinearOperator) -> None:
         super().__init__(dtype=Op.dtype, dims=Op.dims, dimsd=Op.dimsd, name=Op.name)
         self._matvec = jax.jit(Op._matvec)
         self._rmatvec = jax.jit(Op._rmatvec)
 
-    def _rmatvecad(self, x, y):
+    def _rmatvecad(self, x: JaxType, y: JaxType) -> JaxType:
+        """Vector-Jacobian products
+
+        JIT-compiled Vector-Jacobian product
+
+        Parameters
+        ----------
+        x : :obj:`jaxlib.xla_extension.ArrayImpl`
+            Input array
+        y : :obj:`jaxlib.xla_extension.ArrayImpl`
+            Output array (where to store the
+            Vector-Jacobian product)
+
+        Returns
+        ----------
+        y : :obj:`jaxlib.xla_extension.ArrayImpl`
+            Output array
+
+        """
         _, f_vjp = jax.vjp(self._matvec, x)
         return jax.jit(f_vjp)(y)
