6af08b649b7345f5fe99456123c7bb50c7dff689
mrava87 <matteoravasi@gmail.com>
2024-07-05 20:57:33 +0100

    minor: fix typo in fredholm1 docstring

--- pylops/signalprocessing/fredholm1.py
+++ pylops/signalprocessing/fredholm1.py
@@ -61,7 +61,7 @@ class Fredholm1(LinearOperator):
         d(k, x, z) = \int{G(k, x, y) m(k, y, z) \,\mathrm{d}y}
         \quad \forall k=1,\ldots,n_{slice}
 
-    on the other hand its adjoin is expressed as
+    on the other hand its adjoint is expressed as
 
     .. math::
 
