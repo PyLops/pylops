28cc528abeed5a07d1906e692be4dc4b7e868fff
mrava87 <matteoravasi@gmail.com>
2024-07-26 23:17:55 +0300

    test: add safeguards to dtcwt and spgl1 for numpy2

--- pytests/test_dtcwt.py
+++ pytests/test_dtcwt.py
@@ -3,6 +3,9 @@ import pytest
 
 from pylops.signalprocessing import DTCWT
 
+# currently test only if numpy<2.0.0 is installed...
+np_version = np.__version__.split(".")
+
 par1 = {"ny": 10, "nx": 10, "dtype": "float64"}
 par2 = {"ny": 50, "nx": 50, "dtype": "float64"}
 
@@ -17,66 +20,67 @@ def sequential_array(shape):
 @pytest.mark.parametrize("par", [(par1), (par2)])
 def test_dtcwt1D_input1D(par):
     """Test for DTCWT with 1D input"""
+    if int(np_version[0]) < 2:
+        t = sequential_array((par["ny"],))
 
-    t = sequential_array((par["ny"],))
+        for level in range(1, 10):
+            Dtcwt = DTCWT(dims=t.shape, level=level, dtype=par["dtype"])
+            x = Dtcwt @ t
+            y = Dtcwt.H @ x
 
-    for level in range(1, 10):
-        Dtcwt = DTCWT(dims=t.shape, level=level, dtype=par["dtype"])
-        x = Dtcwt @ t
-        y = Dtcwt.H @ x
-
-        np.testing.assert_allclose(t, y)
+            np.testing.assert_allclose(t, y)
 
 
 @pytest.mark.parametrize("par", [(par1), (par2)])
 def test_dtcwt1D_input2D(par):
     """Test for DTCWT with 2D input (forward-inverse pair)"""
-
-    t = sequential_array(
-        (
-            par["ny"],
-            par["ny"],
+    if int(np_version[0]) < 2:
+        t = sequential_array(
+            (
+                par["ny"],
+                par["ny"],
+            )
         )
-    )
 
-    for level in range(1, 10):
-        Dtcwt = DTCWT(dims=t.shape, level=level, dtype=par["dtype"])
-        x = Dtcwt @ t
-        y = Dtcwt.H @ x
+        for level in range(1, 10):
+            Dtcwt = DTCWT(dims=t.shape, level=level, dtype=par["dtype"])
+            x = Dtcwt @ t
+            y = Dtcwt.H @ x
 
-        np.testing.assert_allclose(t, y)
+            np.testing.assert_allclose(t, y)
 
 
 @pytest.mark.parametrize("par", [(par1), (par2)])
 def test_dtcwt1D_input3D(par):
     """Test for DTCWT with 3D input (forward-inverse pair)"""
+    if int(np_version[0]) < 2:
+        t = sequential_array((par["ny"], par["ny"], par["ny"]))
 
-    t = sequential_array((par["ny"], par["ny"], par["ny"]))
+        for level in range(1, 10):
+            Dtcwt = DTCWT(dims=t.shape, level=level, dtype=par["dtype"])
+            x = Dtcwt @ t
+            y = Dtcwt.H @ x
 
-    for level in range(1, 10):
-        Dtcwt = DTCWT(dims=t.shape, level=level, dtype=par["dtype"])
-        x = Dtcwt @ t
-        y = Dtcwt.H @ x
-
-        np.testing.assert_allclose(t, y)
+            np.testing.assert_allclose(t, y)
 
 
 @pytest.mark.parametrize("par", [(par1), (par2)])
 def test_dtcwt1D_birot(par):
     """Test for DTCWT birot (forward-inverse pair)"""
-    birots = ["antonini", "legall", "near_sym_a", "near_sym_b"]
+    if int(np_version[0]) < 2:
+        birots = ["antonini", "legall", "near_sym_a", "near_sym_b"]
 
-    t = sequential_array(
-        (
-            par["ny"],
-            par["ny"],
+        t = sequential_array(
+            (
+                par["ny"],
+                par["ny"],
+            )
         )
-    )
 
-    for _b in birots:
-        print(f"birot {_b}")
-        Dtcwt = DTCWT(dims=t.shape, biort=_b, dtype=par["dtype"])
-        x = Dtcwt @ t
-        y = Dtcwt.H @ x
+        for _b in birots:
+            print(f"birot {_b}")
+            Dtcwt = DTCWT(dims=t.shape, biort=_b, dtype=par["dtype"])
+            x = Dtcwt @ t
+            y = Dtcwt.H @ x
 
-        np.testing.assert_allclose(t, y)
+            np.testing.assert_allclose(t, y)
