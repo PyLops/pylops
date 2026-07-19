eb37f36e1daa017c55884e62026a7ffd54900470
mrava87 <matteoravasi@gmail.com>
2026-07-19 23:50:13 +0100

    doc: added example with transposition with UDCT

--- examples/plot_udct.py
+++ examples/plot_udct.py
@@ -49,7 +49,7 @@ fig.tight_layout()
 ###############################################################################
 # Next, the 2D version of the UDCT can also be applied along the last two axes
 # of a N-dimensional input. This can be easily done with the aid of the
-# :py:class:`pylops.basicoperators.Kronecker` operator.
+# :py:class:`pylops.Kronecker` operator.
 
 shape = (2, 3, 64, 32)
 x = make_zone_plate(shape[2:])
@@ -77,6 +77,40 @@ for ax in axs:
     ax.axis("tight")
     ax.axis("off")
 fig.tight_layout()
+
+
+###############################################################################
+# And similarly, if we want to apply the UDCT over any two axes, the
+# :py:class:`pylops.Transpose` can be used to re-organize the
+# axes of the input prior to applying the transform
+shape = (2, 64, 3, 32)
+x = make_zone_plate((shape[1], shape[3]))
+
+# replicate over the first two axes.
+x = np.tile(x[None, :, None], (shape[0], 1, shape[2], 1))
+
+Cop = pylops.signalprocessing.UDCT(dims=(shape[1], shape[3]))
+Top = pylops.Transpose(shape, axes=(0, 2, 1, 3))
+Iop = pylops.Identity(prod((shape[0], shape[2])))
+Cop = pylops.Kronecker(Iop, Cop) @ Top
+
+y = Cop @ x.ravel()
+xinv = (Cop.H @ y).reshape(shape)
+
+fig, axs = plt.subplots(1, 3, figsize=(10, 4))
+axs[0].imshow(np.mean(x, axis=(0, 2)), cmap="gray", vmin=-1, vmax=1)
+axs[0].set_title("Input")
+axs[1].imshow(np.mean(xinv, axis=(0, 2)), cmap="gray", vmin=-1, vmax=1)
+axs[1].set_title("Round-trip")
+axs[2].imshow(
+    np.mean(x, axis=(0, 2)) - np.mean(xinv, axis=(0, 2)), cmap="gray", vmin=-1, vmax=1
+)
+axs[2].set_title("Difference")
+for ax in axs:
+    ax.axis("tight")
+    ax.axis("off")
+fig.tight_layout()
+
 
 ###############################################################################
 # Finally, if the :py:class:`pylops.signalprocessing.UDCT` is applied directly
